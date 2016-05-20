5e66aeaddd9ca0ffbcceedd83a1d7bbf828d6374
Kincy Clark <source@kincy.com>
2016-05-19 21:00:19 -0700

    updated with requests

--- user_deleter.py
+++ user_deleter.py
@@ -14,6 +14,7 @@ import argparse
 import logging
 import http.client
 import argparse
+import requests
 
 __author__="kincy"
 __date__ ="$Jan 11, 2011 10:18:04 AM$"
@@ -40,8 +41,16 @@ def getList(objectType, pageNum):
 	data = {}
 	data["method"] = "object.query"
 	data["id"] = 1
-	data["params"] = ['SELECT * FROM ' + objectType + ' WHERE area_status = \'disabled\' PAGE ' + str(pageNum)]
-	return data
+	data["params"] = ['SELECT * FROM ' + objectType + ' PAGE ' + str(pageNum)]
+	result = callAPI("getList", data)
+	if not result:
+		return False
+	
+	if len(result['result']['items']) == 0:
+		writeMasheryError("getList", key, "Objects not found in area")
+		return False 
+	
+	return result['result']['items'][0]
 	
 
 def deleteMember(memberID):
@@ -49,30 +58,91 @@ def deleteMember(memberID):
 	data["method"] = "member.delete"
 	data["id"] = 1
 	data["params"] = [memberID]
-	# create the json payload for the API call
-		# print (data)
-	return data
+	result = callAPI("deleteMember", data)
+	if not result:
+		return False
+	
+	if len(result['result']['items']) == 0:
+		writeMasheryError("deleteMember", key, "Member not found in area")
+		return False 
 
-def callAPI(data):
+	return result['result']['items'][0]
+
+def fetchKey(key):
+	data = {}
+	data["method"] = "object.query"
+	data["id"] = 1
+	data["params"] = ["SELECT * FROM keys WHERE apikey = '" + key +"'"]	
+	result = callAPI("fetchKey", data)
+	if not result:
+		return False
+	
+	if len(result['result']['items']) == 0:
+		writeMasheryError("fetchKey", key, "Key not found in Mashery database")
+		return False 
+	
+	logging.debug("Successfully fetched key: " + key)
+	logging.debug(result['result']['items'][0])
+	return result['result']['items'][0]
+
+def callAPI(operation, data):
+
 	# convert the dict to a JSON payload
-	data = json.dumps(data, ensure_ascii=True) # , sort_keys = True, indent=4)
-	data = data.encode('utf-8')
+	data = json.dumps(data, ensure_ascii=True).encode('utf-8') # , sort_keys = True, indent=4)
+	# generate the URL to call the Mashery API
 	url = endpoint + path + "?apikey=" + developers['apikey'] + "&sig=" + buildAuthParams()
-	print (url)
-	req = urllib.request.Request(url, data)
-	# print (url)
-	try:
-		response = urllib.request.urlopen(req)
-		return response.read()
-		
-	except urllib.error.URLError as e:
-		pprint.pprint(data)
-		print (e.code)
-		print (e.reason)
-		print (e.args)
-		return e.reason.encode("utf-8")
-		# sys.exit()
 
+	try:
+		response = requests.post(url, data, timeout=30)
+	
+	except requests.exceptions.RequestException as e:
+		# we have to decode the json object for pretty printing
+		# Otherwise, just outputs a byte object that isn't pretty printable
+		errorOutput = {}
+		errorOutput['operation'] = operation
+		errorOutput["message"] = str(e)
+		errorOutput["oldapikey"] = oldApiKey.rstrip()
+		errorOutput["time"] = str(datetime.datetime.now().time())
+		logging.warn(errorOutput)
+		with open(keyErrorsFile, "a") as errorFile:
+			errorFile.write(str(errorOutput) + "\n")
+		time.sleep(5)
+		return False
+
+	logging.debug("RESPONSE HEADERS")
+	logging.debug(response.headers)
+	
+	if response.headers['Content-Length'] == 0:
+		logging.warn("Zero-byte Content Returned")
+		writeMasheryError("fetchKey", key, "Key not found in Mashery database")
+		time.sleep(2)
+		return False
+
+	try:
+		return response.json()
+	except (ValueError, TypeError):
+		errorOutput = {}
+		errorOutput['operation'] = operation
+		errorOutput["message"] = "No JSON Response" + str(response.headers)
+		errorOutput["oldapikey"] = oldApiKey.rstrip()
+		errorOutput["time"] = str(datetime.datetime.now().time())
+		logging.warn(errorOutput)
+		with open(keyErrorsFile, "a") as errorFile:
+			errorFile.write(str(errorOutput) + "\n")
+		time.sleep(2)
+		return False
+		
+	return response.json()
+	
+def writeMasheryError(operation, apikey, reason):
+		errorOutput = {}
+		errorOutput['message'] = reason
+		errorOutput['oldapikey'] = oldApiKey.rstrip()
+		errorOutput["time"] = str(datetime.datetime.now().time())
+		errorOutput['operation'] = operation
+		logging.warn(errorOutput)
+		with open(keyErrorsFile, "a") as errorFile:
+			errorFile.write(str(errorOutput) + "\n")
 	
 
 if __name__ == "__main__":
